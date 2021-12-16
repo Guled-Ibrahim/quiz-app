5773a5628c131a53e36c8291bcaf1436e8664622
Guled  Ibrahim <gman_000@yahoo.com>
2021-12-16 17:30:15 +0000

    added subtext

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ function App() {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
-      className='h-screen w-screen'
-    ></div>
+      className='h-screen w-screen lg:flex lg:items-center lg:flex-col'
+    >
+      <div className='bg-white lg:rounded-lg lg:w-4/12 lg:relative mt-56 '>
+        <p className='capitalize lg:font-extrabold text-white lg:text-3xl lg:absolute lg:bottom-6 lg:left-0'>
+          capital quiz
+        </p>
+        test
+      </div>
+    </div>
   );
 }
 
