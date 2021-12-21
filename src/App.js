53b5d2d28428a96362c4ecff15beec52e61e3494
Guled  Ibrahim <gman_000@yahoo.com>
2021-12-21 17:55:41 +0000

    next button is shown when clicked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import background from './assets/background.png';
 import man from './assets/man.svg';
 import Answer from './components/Answer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
+import axios from 'axios';
+
 function App() {
-  const [loading, isLoading] = useState(true);
+  const [loading, isLoading] = useState(false);
+  const [countryList, setCountryList] = useState([
+    'peru',
+    'morocco',
+    'finland',
+    'russia',
+    'japan',
+  ]);
+  const [index, setIndex] = useState(1);
+  const [country, setCountry] = useState();
+  const [city, setCity] = useState();
+  const [showButton, isShowButton] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.com/v3.1/name/${countryList[index]}`)
+      .then(({ data }) => {
+        setCity(data[0].capital[0]);
+        setCountry(countryList[index]);
+      });
+  }, [index]);
+
   return (
     <div
       style={{
@@ -25,14 +48,26 @@ function App() {
           <img src={man} alt='' className='lg:absolute lg:-top-24 right-0' />
           <div className='lg:flex lg:flex-col lg:mx-2 lg:mb-4'>
             <p className='text-purple-900 lg:font-extrabold lg:text-2xl lg:mt-12 lg:ml-4 lg:mb-8'>
-              Kuala lumpar is the capital city of
+              {`${city} is the capital city of`}
             </p>
             <div className='lg:flex lg:flex-col'>
-              <Answer answer={'vietnam'} order={'A'} />
+              <Answer
+                answer={country}
+                order={'A'}
+                toggleShow={(toggle) => isShowButton(toggle)}
+              />
               <Answer answer={'malaysia'} order={'B'} />
               <Answer answer={'sweden'} order={'C'} />
               <Answer answer={'austria'} order={'D'} />
             </div>
+            {showButton && (
+              <button
+                onClick={() => setIndex((prev) => prev + 1)}
+                className='lg:bg-orange-400 lg:text-white lg:self-end lg:py-4 lg:px-2 lg:rounded-md lg:mx-4 lg:w-24 lg:text-xl lg:mt-4 lg:mb-4'
+              >
+                next
+              </button>
+            )}
           </div>
         </div>
       )}
--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -1,7 +1,9 @@
-const Answer = ({ answer, order }) => {
+const Answer = ({ answer, order, toggleShow }) => {
   return (
     <button
-      onClick={(e) => console.log(e.target.innerText)}
+      onClick={(e) => {
+        toggleShow(true);
+      }}
       className='lg:text-left lg:mx-4 lg:text-purple-600 lg:border-2 lg:border-purple-500 lg:hover:bg-orange-400 lg:hover:text-white lg:py-4 lg:my-4 lg:rounded-md lg:hover:border-orange-400 lg:capitalize'
     >
       <span className='lg:font-extrabold lg:ml-4 lg:mr-12'>{order}</span>
